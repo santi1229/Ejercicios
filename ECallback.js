34c7688518ad03b8b9933d2afbb0f6904604f51c
Santiago <santyagoplay@gmail.com>
2025-09-24 20:25:13 -0500

    Archivo completo

--- ECallback.js
+++ ECallback.js
@@ -1,3 +1,14 @@
+function validarUsuarios(usuarios, callback) {
+    return usuarios.filter(callback);
+}
 
+let usuarios = [
+  { nombre: "Ana", edad: 25 },
+  { nombre: "Pedro", edad: 17 },
+  { nombre: "Maria", edad: 30 },
+  { nombre: "Andrés", edad: 15 },
+  { nombre: "Lucía", edad: 20 }
+];
 
-
+console.log(validarUsuarios(usuarios, u => u.edad >= 18));
+console.log(validarUsuarios(usuarios, u => u.nombre.startsWith("A")));
